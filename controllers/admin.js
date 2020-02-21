e9e0cdd188c31c55ce1f94379fa71c4c3dfe6207
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-21 11:29:43 -0300

    admin: ready screens, part of the configured routes, new data, but still problems to solve

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
+const data = require('../data.json');
 const receipts = require('../data');
 
 //Listing receipts (Mostrar a lista de receitas)
 exports.receipts = function(req,res) {
-    return res.render("admin/home", {receipts});
+    return res.render("admin/home", {receipts: data.receipts});
 };
 
 //Create recipe (Mostrar o formulário de nova receita)
@@ -23,20 +25,91 @@ exports.show = function(req,res) {
 
 //Edit recipe (Mostrar formulário de edição de uma receita)
 exports.edit = function(req,res) {
-    return res.render("admin/edit", {receipts});
+    const { id } = req.params;
+
+    const foundRecipe = data.receipts.find(function(recipe) {
+        return recipe.id == id;
+    });
+
+    if(!foundRecipe) return res.send("recipe not found.");
+
+    const recipe = {
+        ...foundRecipe
+    }
+
+    return res.render("admin/edit", {recipe});
 };
 
 //New recipe
 exports.post = function(req,res) {
-    
+    const keys = Object.keys(req.body);
+
+    console.log(keys);
+
+    for (key of keys) {
+        if(req.body[key] == "") {
+            return res.send("Please, fill all fields.");
+        }
+    }
+
+    let id = 1;
+    const lastRecipe = data.receipts[data.receipts.length - 1];
+
+    if(lastRecipe) {
+        id = lastRecipe.id + 1;
+    }
+
+    const recipe = {
+        id,
+        ...req.body
+    }
+
+    fs.writeFile("data.json", JSON.stringify(data,null,2), function(err) {
+        if(err) return res.send("write file error.");
+
+        return res.redirect("/admin/receipts");
+    });
 };
 
 //Update recipe
 exports.put = function(req,res) {
+    const { id } = req.body;
+    let index = 0;
 
+    const foundRecipe = data.receipts.find(function(recipe, foundIndex) {
+        if (id == recipe.id) {
+            index = foundIndex;
+            return true;
+        }
+    })
+
+    const recipe = {
+        ...foundRecipe,
+        ...req.body,
+    }
+
+    data.receipts[foundIndex] = recipe;
+
+    fs.writeFile("data.json", JSON.stringify(data,null,2), function(err) {
+        if (err) return res.send("write file error.");
+
+        return res.redirect(`/admin/receipts/${id}`);
+    });
 };
 
 //Delete recipe
 exports.delete = function(req,res) {
+    const { id } = req.body;
 
+    const filteredReceipts = data.receipts.filter(function(recipe) {
+        return recipe.id != id;
+    })
+
+    data.receipts = filteredReceipts;
+
+    fs.writeFile("data.json", JSON.stringify(data,null,2), function(err) {
+        if(err) return res.send("write file error.");
+    
+        return res.redirect("/admin/receipts");
+    });
 };
